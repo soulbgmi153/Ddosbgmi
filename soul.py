fc9a21b03899d59c615e7a7066643875ff6234d3
soulbgmi153 <pubgabgh@gmail.com>
2024-11-30 17:40:08 +0100

    Update soul.py

--- soul.py
+++ soul.py
@@ -16,7 +16,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = ''
+TOKEN = '7623124405:AAF_moP794ap1EmBIdB91Ct8ozNIlyZ1lVE'
 MONGO_URI = 'mongodb+srv://Bishal:Bishal@bishal.dffybpx.mongodb.net/?retryWrites=true&w=majority&appName=Bishal'
 FORWARD_CHANNEL_ID = -1002241427670
 CHANNEL_ID = -1002241427670
@@ -28,7 +28,7 @@ client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
 db = client['zoya']
 users_collection = db.users
 
-bot = telebot.TeleBot(7623124405:AAF_moP794ap1EmBIdB91Ct8ozNIlyZ1lVE)
+bot = telebot.TeleBot()
 REQUEST_INTERVAL = 1
 
 blocked_ports = [8700, 20000, 443, 17500, 9031, 20002, 20001]  # Blocked ports list
